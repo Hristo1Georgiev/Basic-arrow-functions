cf4162dbefd0151d823834957eecff59a5209154
Hristo Georgiev <hristo1georgiev@yahoo.com>
2021-07-28 10:41:32 +0200

    Opdracht Basic arrow functions

---
+++ script.js
@@ -0,0 +1,26 @@
+// zet de functie om naar een arrow functie
+//deel A
+const ikRockArrowFunctions = function () {
+    console.log("Joe, ik rock de arrow functions!");
+};
+ikRockArrowFunctions();
+
+// deel B
+const ikRockArrowFunctions2 = () => console.log("Joe, ik rock de arrow functions!")
+ikRockArrowFunctions2()
+
+// tweede opdracht
+const fivePlusSeven = function () {
+    return 5 + 7
+};
+console.log(fivePlusSeven());
+
+//Deel C
+const myFunction = (x, y) => x + y
+
+//Deel D
+const addFive = a => a + 5
+
+//Deel E
+createObject = () =>"gereeting: 'hoi'"
+console.log(createObject());
